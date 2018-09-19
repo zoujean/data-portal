d6660d0a2af4e9d5cabd68e9d5f57455b860b91e
abgeorge7 <abgeorge@umich.edu>
2018-09-19 10:57:03 -0500

    wip - no data showing up

--- src/arrangerApi.js
+++ src/arrangerApi.js
@@ -3,11 +3,31 @@ import urlJoin from 'url-join';
 
 /* File from @arranger node module - customized to pass additional params in fetch request */
 
-const alwaysSendHeaders = { 'Content-Type': 'application/json' };
+export const getCookie = name => {
+  if (!document.cookie) {
+    return null;
+  }
+  const xsrfCookies = document.cookie.split(';')
+    .map(c => c.trim())
+    .filter(c => c.startsWith(name + '='));
+  if (xsrfCookies.length === 0) {
+    return null;
+  }
+  return decodeURIComponent(xsrfCookies[0].split('=')[1]);
+}
 
-const api = ({ endpoint = '', body, headers }) => fetch(urlJoin(ARRANGER_API, endpoint), {
+const alwaysSendHeaders = { 'Content-Type': 'application/json' };
+const csrf = getCookie('csrf');
+const accessToken = sessionStorage.getItem('token');
+const tokenHeaders = { 'Authorization': `Bearer ${accessToken}` };
+
+const api = ({ endpoint = '', body, headers }) => fetch(urlJoin('https://qa-brain.planx-pla.net/api/v0/flat-search', endpoint), {
   method: 'POST',
-  headers: { ...alwaysSendHeaders, ...headers },
+  headers: {
+    ...alwaysSendHeaders,
+    ...tokenHeaders,
+    ...headers
+  },
   body: JSON.stringify(body),
   credentials: 'include',
 }).then(r => r.json());
