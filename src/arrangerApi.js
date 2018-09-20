126c0dcc6f06c1f01e11893b233b3c8bc2b43306
abgeorge7 <abgeorge@umich.edu>
2018-09-20 09:48:34 -0500

    content security

--- src/arrangerApi.js
+++ src/arrangerApi.js
@@ -1,5 +1,6 @@
 import { ARRANGER_API } from '@arranger/components/dist/utils/config';
 import urlJoin from 'url-join';
+import { params } from '../data/parameters';
 
 /* File from @arranger node module - customized to pass additional params in fetch request */
 
@@ -19,17 +20,33 @@ export const getCookie = name => {
 const alwaysSendHeaders = { 'Content-Type': 'application/json' };
 const csrf = getCookie('csrf');
 const accessToken = sessionStorage.getItem('token');
-const tokenHeaders = { 'Authorization': `Bearer ${accessToken}` };
+const tokenHeaders = { 'Authorization': `bearer ${accessToken}` };
+let urls = {
+  bhc: 'https://qa-brain.planx-pla.net/api/v0/flat-search',
+  default: 'https://abby.planx-pla.net/api/v0/flat-search',
+};
 
-const api = ({ endpoint = '', body, headers }) => fetch(urlJoin('https://qa-brain.planx-pla.net/api/v0/flat-search', endpoint), {
-  method: 'POST',
-  headers: {
-    ...alwaysSendHeaders,
-    ...tokenHeaders,
-    ...headers
-  },
-  body: JSON.stringify(body),
-  credentials: 'include',
-}).then(r => r.json());
+let projects = [
+  'bhc',
+  'default',
+];
+
+const api = ({ endpoint = '', body, headers }) => {
+  let json = [];
+  let parsedUrl = endpoint.split('/');
+  let newUrl = parsedUrl[parsedUrl.length-1];
+  return fetch(urlJoin('https://qa-brain.planx-pla.net/api/v0/flat-search', endpoint), {
+      method: 'POST',
+      headers: {
+        ...alwaysSendHeaders,
+        ...tokenHeaders,
+        ...headers
+      },
+      body: JSON.stringify(body),
+      credentials: 'include',
+    }).then(r => {
+      json.push(r.json());
+    });
+  };
 
 export default api;
