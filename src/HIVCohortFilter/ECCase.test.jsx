0289a4e1602b1a1efc9300863a84a4a39eb3c343
Zakir Gowani <zakirgowani@gmail.com>
2019-05-10 14:03:07 -0500

    test: NDH App

---
+++ src/HIVCohortFilter/ECCase.test.jsx
@@ -0,0 +1,38 @@
+import React from 'react';
+// import { mount } from 'enzyme';
+// import * as d3 from 'd3-selection';
+
+import ECCase from './ECCase';
+// import { buildTestData } from '../GraphUtils/testData';
+
+describe('the ECCase', () => {
+  /*
+   * EC: Elite Controller. These are patients with the ability to
+   * spontaneously control HIV viral loads for prolonged periods of time.
+  */
+  it('classifies EC Case correctly (1)', () => {
+    
+    expect( ).toBe( );
+    expect( ).toBe( );
+  });
+
+  it('classifies EC Case correctly (2)', () => {
+    
+    expect( ).toBe( );
+    expect( ).toBe( );
+  });
+
+  it('classifies control case correctly (1)', () => {
+    
+    expect( ).toBe( );
+    expect( ).toBe( );
+  });
+
+  it('classifies control case correctly (2)', () => {
+    
+    expect( ).toBe( );
+    expect( ).toBe( );
+  });
+
+
+});
