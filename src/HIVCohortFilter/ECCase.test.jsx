eb49205e384d07a170e0ce9b73bdb7008ea6148e
Zakir Gowani <zakirgowani@gmail.com>
2019-05-13 15:34:53 -0500

    squash

--- src/HIVCohortFilter/ECCase.test.jsx
+++ src/HIVCohortFilter/ECCase.test.jsx
@@ -5,12 +5,45 @@ import React from 'react';
 import ECCase from './ECCase';
 // import { buildTestData } from '../GraphUtils/testData';
 
+function mockFollowup(
+  subjectID, visitNumber, thrpyv, visitDate, fposdate, frstdthd, leu3n, viralLoad
+  ) {
+    return {
+          "subject_id": subjectID,
+          "visit_number": visitNumber,
+          "thrpyv": thrpyv,
+          "visit_date": visitDate,
+          "fposdate": fposdate,
+          "frstdthd": frstdthd,
+          "leu3n": leu3n,
+          "submitter_id": "x",
+          "viral_load": viralLoad
+        };
+}
+
 describe('the ECCase', () => {
   /*
    * EC: Elite Controller. These are patients with the ability to
    * spontaneously control HIV viral loads for prolonged periods of time.
   */
   it('classifies EC Case correctly (1)', () => {
+    /*
+      Case 1: Patient with no HAART treatments, Y months of consecutive, adjacent followups
+      that all have viral_load < X should be classified as EC
+    */
+    let X = 4000;
+    let Y = 18;
+    const subjectToVisitMap = {
+      "subject-1" : [ 
+        mockFollowup("subject-1", 1, null, 1900, 1900, 1920, null, 4005),
+        // consecutive viral loads below threshold begin here
+        mockFollowup("subject-1", 2, null, 1900, 1900, 1920, null, 3000),
+        mockFollowup("subject-1", 3, null, 1900, 1900, 1920, null, 2000),
+        mockFollowup("subject-1", 4, null, 1900, 1900, 1920, null, 3999),
+        // the 18 month window is above; the below followup makes the subject EC
+        mockFollowup("subject-1", 5, null, 1900, 1900, 1920, null, 3800),
+      ]
+    };
     
     expect( ).toBe( );
     expect( ).toBe( );
